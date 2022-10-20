799ce2596d44e647799885ea9eef9ebc4e28f7ad
Vijay-raj03 <112714943+Vijay-raj03@users.noreply.github.com>
2022-10-20 09:29:55 +0530

    Create LOAVES.PY

---
+++ LOAVES.PY
@@ -0,0 +1,8 @@
+x=int(input("Enter the Number of new loaves purchased : "))
+y=int(input("Enter the Number of old loaves purchased : "))
+a=(0.6*185)*y
+b=185*x
+print("Regular Price : Rs. 185")
+print("Old loaves Amount : Rs. ",a)
+print("New loaves Amount : Rs. ",b)
+print("Total Amount : Rs. ",a+b)
